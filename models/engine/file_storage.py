8532d6a78c1f145789b175d3a64b42d74025b593
Nickson K Rop <nickyrutto@gmail.com>
2024-02-20 23:43:54 -0800

    add a new public method to delete an object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return self.__objects
+        elif type(cls) == str:
+            return {k: v for k, v in self.__objects.items()
+                    if v.__class__.__name__ == cls}
+        else:
+            return {k: v for k, v in self.__objects.items()
+                    if v.__class__ == cls}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +31,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+    
+    def delete(self, obj=None):
+        """deletes obj from __objects if it's inside"""
+        if obj != None:
+            del self.__objects[obj.__class__.__name__ + "." + obj.id]
+            self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
