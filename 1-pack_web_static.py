9ad622a0d2e53ee05b555d70564b2d1361ca1c57
Nickson K Rop <nickyrutto@gmail.com>
2024-03-10 13:11:21 -0700

    fixed pycodestyle error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,11 +2,11 @@
 """
 This fabric script generates a .tgz archive from the folder web_static
 """
-
-
 from fabric.api import local
 from time import strftime
 from datetime import datetime
+
+
 def do_pack():
     """
     This function generates a .tgz archive
@@ -15,9 +15,8 @@ def do_pack():
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-                .format(filename))
+              .format(filename))
         return "versions/web_static_{}.tgz".format(filename)
 
     except Exception as e:
         return None
-
