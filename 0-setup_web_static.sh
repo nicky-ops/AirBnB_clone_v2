f1b97539f96e7a52ac61875dc1bf3def1860a12e
Nickson K Rop <nickyrutto@gmail.com>
2024-03-10 07:57:30 -0700

    fix some errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,22 +6,17 @@ if ! command -v ngnix &> /dev/null; then
 fi
 
 sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo chown -R ubuntu:ubuntu /data/
-
+sudo touch /data/web_static/releases/test/index.html
 echo "<html>
 <head>
 	<title> Test Page </title>
 </head>
 <body>
-	<h1>This is a test page</h1>
-	<p> This page is used to test configuration. </p>
+	<p> Holberton School </p>
 </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-nginx_config="/etc/nginx/sites-available/default"
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' $nginx_config
-
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
