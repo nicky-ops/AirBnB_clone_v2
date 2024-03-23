09ac83e06d596a3aa251c7aac5e26bab4ed2397b
Nickson K Rop <nickyrutto@gmail.com>
2024-03-23 05:32:47 -0700

    a new route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+THis script starts a flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    returns Hello HBNB
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    displays HBNB
+    '''
+    return 'HBNB'
+
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """
+    display C followed by text
+    """
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
