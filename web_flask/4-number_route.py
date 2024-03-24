d86283fc70bd9fbdc5efb7a473850f21e00c9f91
Nickson K Rop <nickyrutto@gmail.com>
2024-03-24 05:05:44 -0700

    new route to display n if it is an integer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,51 @@
 #!/usr/bin/python3
+"""
+THis script starts a flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    returns Hello HBNB
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    displays HBNB
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """
+    display C followed by text
+    """
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """
+    route to display python is cool
+    """
+    return 'Python {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
