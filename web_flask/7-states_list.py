f31e921a7a6ce6bb8425b1a738e39082b862b9f0
Nickson K Rop <nickyrutto@gmail.com>
2024-03-27 09:09:11 -0700

    states route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-This script starts a Flask web application
-"""
-from flask import Flask, render template
+"""This script starts a Flask web application"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 
+
 app = Flask(__name__)
+
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -22,6 +22,7 @@ def display_states():
 def teardown(self):
     """Method to remove current SQLAlchemy Session"""
     storage.close()
+
 
 
 if __name__ == "__main__":
